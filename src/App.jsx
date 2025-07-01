9a607fdae7ab399185cd6faf4eb354006ef18abe
Hugo M. Ruiz-Mireles <hugo.ruizmireles@gmail.com>
2025-06-30 17:18:07 -0700

    init commit
    Hoping to make this thing work and if not, at least I'll learn a lot of stuff. Basic Vite React set up with a bit of tailwind and some basic text.

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="bg-cyan-500 text-green p-8 text-center">
+      <h1 className="text-4xl font-bold">DSA Visualizer</h1>
+      
+    </div>
+  )
+}
+
+export default App
